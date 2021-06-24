58448eea2ddd285f9ffe95ee9fe3c4cf19a1937b
Kundan Kumar <kundanvrs@gmail.com>
2021-06-24 19:30:49 +0530

    cineworld update

--- CINEMAWORLD/CineWorld/CineWorld/urls.py
+++ CINEMAWORLD/CineWorld/CineWorld/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+import movies
+
 
 urlpatterns = [
+    path('movies/', include('movies.urls')),
+    path('userprofile/', include('userprofile.urls')),
     path('admin/', admin.site.urls),
+    path('',include('movies.urls')),
 ]
+
+from django.contrib import settings
+from django.contrib.urls.static import static
+
+if.settings.DEBUG:
+    urlpatterns = static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
